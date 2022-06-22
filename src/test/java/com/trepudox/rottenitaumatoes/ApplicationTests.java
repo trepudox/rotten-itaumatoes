0d3310423fd34363d192a40900462b609741a888
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-06-22 20:17:03 -0300

    commit inicial

---
+++ src/test/java/com/trepudox/rottenitaumatoes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trepudox.rottenitaumatoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
