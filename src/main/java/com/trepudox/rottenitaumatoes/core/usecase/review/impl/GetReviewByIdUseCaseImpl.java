92bbc8e6e90fd3f3322e06d6cc9ce7ebb3489daf
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-06-27 23:02:13 -0300

    Adicionando excecao personalizada caso nao encontrar review

--- src/main/java/com/trepudox/rottenitaumatoes/core/usecase/review/impl/GetReviewByIdUseCaseImpl.java
+++ src/main/java/com/trepudox/rottenitaumatoes/core/usecase/review/impl/GetReviewByIdUseCaseImpl.java
@@ -1,5 +1,6 @@
 package com.trepudox.rottenitaumatoes.core.usecase.review.impl;
 
+import com.trepudox.rottenitaumatoes.core.exception.APIException;
 import com.trepudox.rottenitaumatoes.core.mapper.ReviewMapper;
 import com.trepudox.rottenitaumatoes.core.usecase.review.IGetReviewByIdUseCase;
 import com.trepudox.rottenitaumatoes.dataprovider.dto.ReviewDTO;
@@ -16,7 +17,8 @@ public class GetReviewByIdUseCaseImpl implements IGetReviewByIdUseCase {
 
     @Override
     public ReviewDTO get(Long reviewId) {
-        ReviewModel reviewModel = reviewRepository.findById(reviewId).orElseThrow();
+        ReviewModel reviewModel = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new APIException("Review não existe", "Não foi possível encontrar a review solicitada", 422));
         return ReviewMapper.INSTANCE.reviewModelToReviewDTO(reviewModel);
     }
 }
