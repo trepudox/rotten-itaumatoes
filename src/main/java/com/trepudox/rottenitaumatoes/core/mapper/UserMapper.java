f29f866b661bf6b4927f2caa4339f3a6c06e6116
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-06-30 12:42:21 -0300

    alterando mapper de usuario

--- src/main/java/com/trepudox/rottenitaumatoes/core/mapper/UserMapper.java
+++ src/main/java/com/trepudox/rottenitaumatoes/core/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.trepudox.rottenitaumatoes.core.mapper;
 import com.trepudox.rottenitaumatoes.dataprovider.dto.UserDTO;
 import com.trepudox.rottenitaumatoes.dataprovider.model.UserModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,7 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "updateDateTime", expression = "java(userModel.getUpdateDateTime() == null ? userModel.getCreationDateTime() : userModel.getUpdateDateTime())")
     UserDTO userModelToDTO(UserModel userModel);
 
 }
